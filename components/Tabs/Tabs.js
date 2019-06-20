746635006a6543362a4bfbf9e0c2f3276d5a8a59
bharti3bk <bharti3bk@gmail.com>
2019-06-20 16:21:16 -0400

    Dropdown.js

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -2,31 +2,38 @@
 class TabLink {
   constructor(element) {
     // Assign this.element to the passed in DOM element
-    // this.element;
+    // this.element; 
+
+    this.element = element;
     
     // Get the custom data attribute on the Link
-    // this.data;
+    // this.data; 
+    this.data = this.element.classList.tab;
     
     // Using the custom data attribute get the associated Item element
     // this.itemElement;
-    
+    this.itemElement = new TabItem(this.itemElement);
     // Using the Item element, create a new instance of the TabItem class
     // this.tabItem;
     
     // Add a click event listener on this instance, calling the select method on click
-
+      this.element.addEventListener('click' , () => {
+        this.select();
+      })
   };
 
   select() {
     // Get all of the elements with the tabs-link class
     // const links;
-
+     const links = document.querySelectorAll('tabs-link');
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     // Array.from(links).forEach();
-
+    Array.from(links).forEach((item) => {
+      item.classList.remove('tabs-item-selected');
+    });
     // Add a class named "tabs-link-selected" to this link
     // this.element;
-    
+    this.element.classList.add('tabs-link-selected');
     // Call the select method on the item associated with this link
 
   }
